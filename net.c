8b29c5a673566c875c006fc563fff2fa1b3da7e2
yufunagi <yufunagi@momidi.net>
2025-07-29 15:35:22 +0900

    step9: Upper Layer Protocol Management

--- net.c
+++ net.c
@@ -250,6 +250,7 @@ net_shutdown(void)
 }
 
 #include "ip.h"
+#include "icmp.h"
 
 int
 net_init(void)
@@ -260,6 +261,10 @@ net_init(void)
     }
     if (ip_init() == -1) {
         errorf("ip_init() failure");
+        return -1;
+    }
+    if (icmp_init() == -1) {
+        errorf("icmp_init() failure");
         return -1;
     }
     infof("initialized");
